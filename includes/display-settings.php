849060c2a781cddfebd584bde50430e7ee9a42f8
Ulrich Pogson <grapplerulrich@gmail.com>
2013-06-16 15:30:17 +0200

    -

--- includes/display-settings.php
+++ includes/display-settings.php
@@ -17,6 +17,7 @@
 	<?php screen_icon(); ?>
 	<h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
 
+	<?php settings_errors( 'edd-notices' ); ?>
 	<form method="post" action="options.php">
 		<?php
 		// This prints out all hidden setting fields
