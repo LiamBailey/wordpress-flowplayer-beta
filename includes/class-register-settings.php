f6e262862812656138fc4c452ed3dbfedd386ca6
Ulrich Pogson <grapplerulrich@gmail.com>
2013-06-22 19:18:45 +0200

    change edd to fp5

--- includes/class-register-settings.php
+++ includes/class-register-settings.php
@@ -25,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-function edd_register_settings() {
+function fp5_register_settings() {
 
 	// Setup some default option sets
 	$pages = get_pages();
@@ -37,85 +37,85 @@ function edd_register_settings() {
 	}
 
 /**
-	 * 'Whitelisted' EDD settings, filters are provided for each settings
+	 * 'Whitelisted' fp5 settings, filters are provided for each settings
 	 * section to allow extensions and other plugins to add their own settings
 	 */
-	$edd_settings = array(
+	$fp5_settings = array(
 		/** General Settings */
-		'general' => apply_filters('edd_settings_general',
+		'general' => apply_filters('fp5_settings_general',
 			array(
 				'commercial_version' => array(
 					'id' => 'commercial_version',
-					'name' => '<h4>' . __('Commercial Version', 'edd') . '</h4>',
-					'desc' => __('Commercial version removes the Flowplayer logo and allows you to use your own logo image. You can purchase a license and obtain a license key in flowplayer.org.', 'edd'),
+					'name' => '<h4>' . __('Commercial Version', 'fp5') . '</h4>',
+					'desc' => __('Commercial version removes the Flowplayer logo and allows you to use your own logo image. You can purchase a license and obtain a license key in flowplayer.org.', 'fp5'),
 					'type' => 'header'
 				),
 				'key' => array(
 					'id' => 'key',
-					'name' => __('License Key', 'edd'),
-					'desc' => __('Specify your License Key here.', 'edd'),
+					'name' => __('License Key', 'fp5'),
+					'desc' => __('Specify your License Key here.', 'fp5'),
 					'type' => 'text',
 					'size' => 'small',
 				),
 				'logo' => array(
 					'id' => 'logo',
-					'name' => __( 'Logo', 'edd'),
-					'desc' => __('This is the page buyers are sent to if their transaction is cancelled or fails', 'edd'),
+					'name' => __( 'Logo', 'fp5'),
+					'desc' => __('This is the page buyers are sent to if their transaction is cancelled or fails', 'fp5'),
 					'type' => 'upload',
 				),
 				'logo_origin' => array(
 					'id' => 'logo_origin',
-					'name' => __('Show Logo on this site', 'edd'),
-					'desc' => __('Uncheck this and the logo is only shown in externally embedded players.', 'edd'),
+					'name' => __('Show Logo on this site', 'fp5'),
+					'desc' => __('Uncheck this and the logo is only shown in externally embedded players.', 'fp5'),
 					'type' => 'checkbox'
 				),
 				'video_tracking' => array(
 					'id' => 'video_tracking',
-					'name' => '<strong>' . __('Video Tracking', 'edd') . '</strong>',
-					'desc' => __('Configure the video attribYou can track video traffic using Google Analytics (GA). Visit flowplayer.org for more info', 'edd'),
+					'name' => '<strong>' . __('Video Tracking', 'fp5') . '</strong>',
+					'desc' => __('Configure the video attribYou can track video traffic using Google Analytics (GA). Visit flowplayer.org for more info', 'fp5'),
 					'type' => 'header'
 				),
 				'ga_accountId' => array(
 					'id' => 'ga_accountId',
-					'name' => __('Goofle Analytics account ID', 'edd'),
-					'desc' => __('Specify your GA account ID here.', 'edd'),
+					'name' => __('Goofle Analytics account ID', 'fp5'),
+					'desc' => __('Specify your GA account ID here.', 'fp5'),
 					'type' => 'text',
 					'size' => 'small',
 				),
 				'cdn_options' => array(
 					'id' => 'cdn_options',
-					'name' => '<strong>' . __('CDN Options', 'edd') . '</strong>',
-					'desc' => __('If you want to use a self-hosted copy of Flowplayer instead of the CDN hosted version uncheck Use CDN hosted version? Using the CDN hosted version is preferable in most situations.', 'edd'),
+					'name' => '<strong>' . __('CDN Options', 'fp5') . '</strong>',
+					'desc' => __('If you want to use a self-hosted copy of Flowplayer instead of the CDN hosted version uncheck Use CDN hosted version? Using the CDN hosted version is preferable in most situations.', 'fp5'),
 					'type' => 'header'
 				),
 				'cdn_option' => array(
 					'id' => 'cdn_option',
-					'name' => __('CDN Hosted files', 'edd'),
-					'desc' => __('Uncheck this to load the files from this site', 'edd'),
+					'name' => __('CDN Hosted files', 'fp5'),
+					'desc' => __('Uncheck this to load the files from this site', 'fp5'),
 					'type' => 'checkbox'
 				)
 			)
 		)
 	);
 
-	if ( false == get_option( 'edd_settings_general' ) ) {
-		add_option( 'edd_settings_general' );
+	if ( false == get_option( 'fp5_settings_general' ) ) {
+		add_option( 'fp5_settings_general' );
 	}
 
 	add_settings_section(
-		'edd_settings_general',
-		__( 'General Settings', 'edd' ),
+		'fp5_settings_general',
+		__( 'General Settings', 'fp5' ),
 		'__return_false',
-		'edd_settings_general'
+		'fp5_settings_general'
 	);
 
-	foreach ( $edd_settings['general'] as $option ) {
+	foreach ( $fp5_settings['general'] as $option ) {
 		add_settings_field(
-			'edd_settings_general[' . $option['id'] . ']',
+			'fp5_settings_general[' . $option['id'] . ']',
 			$option['name'],
-			function_exists( 'edd_' . $option['type'] . '_callback' ) ? 'edd_' . $option['type'] . '_callback' : 'edd_missing_callback',
-			'edd_settings_general',
-			'edd_settings_general',
+			function_exists( 'fp5_' . $option['type'] . '_callback' ) ? 'fp5_' . $option['type'] . '_callback' : 'fp5_missing_callback',
+			'fp5_settings_general',
+			'fp5_settings_general',
 			array(
 				'id' => $option['id'],
 				'desc' => $option['desc'],
@@ -129,11 +129,11 @@ function edd_register_settings() {
 	}
 
 	// Creates our settings in the options table
-	register_setting( 'edd_settings_general', 'edd_settings_general', 'edd_settings_sanitize' );
+	register_setting( 'fp5_settings_general', 'fp5_settings_general', 'fp5_settings_sanitize' );
 
 }
 
-add_action( 'admin_init', 'edd_register_settings' );
+add_action( 'admin_init', 'fp5_register_settings' );
 
 /**
  * Header Callback
@@ -144,7 +144,7 @@ add_action( 'admin_init', 'edd_register_settings' );
  * @param array $args Arguments passed by the setting
  * @return void
  */
-function edd_header_callback( $args ) {
+function fp5_header_callback( $args ) {
 	echo '<p class="description">' . $args['desc'] . '</p>';
 }
 
@@ -155,15 +155,15 @@ function edd_header_callback( $args ) {
  *
  * @since 1.0
  * @param array $args Arguments passed by the setting
- * @global $edd_options Array of all the EDD Options
+ * @global $fp5_options Array of all the fp5 Options
  * @return void
  */
-function edd_checkbox_callback( $args ) {
-	global $edd_options;
+function fp5_checkbox_callback( $args ) {
+	global $fp5_options;
 
-	$checked = isset($edd_options[$args['id']]) ? checked(1, $edd_options[$args['id']], false) : '';
-	$html = '<input type="checkbox" id="edd_settings_' . $args['section'] . '[' . $args['id'] . ']" name="edd_settings_' . $args['section'] . '[' . $args['id'] . ']" value="1" ' . $checked . '/>';
-	$html .= '<label for="edd_settings_' . $args['section'] . '[' . $args['id'] . ']"> '  . $args['desc'] . '</label>';
+	$checked = isset($fp5_options[$args['id']]) ? checked(1, $fp5_options[$args['id']], false) : '';
+	$html = '<input type="checkbox" id="fp5_settings_' . $args['section'] . '[' . $args['id'] . ']" name="fp5_settings_' . $args['section'] . '[' . $args['id'] . ']" value="1" ' . $checked . '/>';
+	$html .= '<label for="fp5_settings_' . $args['section'] . '[' . $args['id'] . ']"> '  . $args['desc'] . '</label>';
 
 	echo $html;
 }
@@ -175,16 +175,16 @@ function edd_checkbox_callback( $args ) {
  *
  * @since 1.0
  * @param array $args Arguments passed by the setting
- * @global $edd_options Array of all the EDD Options
+ * @global $fp5_options Array of all the fp5 Options
  * @return void
  */
-function edd_multicheck_callback( $args ) {
-	global $edd_options;
+function fp5_multicheck_callback( $args ) {
+	global $fp5_options;
 
 	foreach( $args['options'] as $key => $option ):
-		if( isset( $edd_options[$args['id']][$key] ) ) { $enabled = $option; } else { $enabled = NULL; }
-		echo '<input name="edd_settings_' . $args['section'] . '[' . $args['id'] . '][' . $key . ']"" id="edd_settings_' . $args['section'] . '[' . $args['id'] . '][' . $key . ']" type="checkbox" value="' . $option . '" ' . checked($option, $enabled, false) . '/>&nbsp;';
-		echo '<label for="edd_settings_' . $args['section'] . '[' . $args['id'] . '][' . $key . ']">' . $option . '</label><br/>';
+		if( isset( $fp5_options[$args['id']][$key] ) ) { $enabled = $option; } else { $enabled = NULL; }
+		echo '<input name="fp5_settings_' . $args['section'] . '[' . $args['id'] . '][' . $key . ']"" id="fp5_settings_' . $args['section'] . '[' . $args['id'] . '][' . $key . ']" type="checkbox" value="' . $option . '" ' . checked($option, $enabled, false) . '/>&nbsp;';
+		echo '<label for="fp5_settings_' . $args['section'] . '[' . $args['id'] . '][' . $key . ']">' . $option . '</label><br/>';
 	endforeach;
 	echo '<p class="description">' . $args['desc'] . '</p>';
 }
@@ -196,22 +196,22 @@ function edd_multicheck_callback( $args ) {
  *
  * @since 1.3.3
  * @param array $args Arguments passed by the setting
- * @global $edd_options Array of all the EDD Options
+ * @global $fp5_options Array of all the fp5 Options
  * @return void
  */
-function edd_radio_callback( $args ) {
-	global $edd_options;
+function fp5_radio_callback( $args ) {
+	global $fp5_options;
 
 	foreach ( $args['options'] as $key => $option ) :
 		$checked = false;
 
-		if ( isset( $edd_options[ $args['id'] ] ) && $edd_options[ $args['id'] ] == $key )
+		if ( isset( $fp5_options[ $args['id'] ] ) && $fp5_options[ $args['id'] ] == $key )
 			$checked = true;
-		elseif( isset( $args['std'] ) && $args['std'] == $key && ! isset( $edd_options[ $args['id'] ] ) )
+		elseif( isset( $args['std'] ) && $args['std'] == $key && ! isset( $fp5_options[ $args['id'] ] ) )
 			$checked = true;
 
-		echo '<input name="edd_settings_' . $args['section'] . '[' . $args['id'] . ']"" id="edd_settings_' . $args['section'] . '[' . $args['id'] . '][' . $key . ']" type="radio" value="' . $key . '" ' . checked(true, $checked, false) . '/>&nbsp;';
-		echo '<label for="edd_settings_' . $args['section'] . '[' . $args['id'] . '][' . $key . ']">' . $option . '</label><br/>';
+		echo '<input name="fp5_settings_' . $args['section'] . '[' . $args['id'] . ']"" id="fp5_settings_' . $args['section'] . '[' . $args['id'] . '][' . $key . ']" type="radio" value="' . $key . '" ' . checked(true, $checked, false) . '/>&nbsp;';
+		echo '<label for="fp5_settings_' . $args['section'] . '[' . $args['id'] . '][' . $key . ']">' . $option . '</label><br/>';
 	endforeach;
 
 	echo '<p class="description">' . $args['desc'] . '</p>';
@@ -224,20 +224,20 @@ function edd_radio_callback( $args ) {
  *
  * @since 1.0
  * @param array $args Arguments passed by the setting
- * @global $edd_options Array of all the EDD Options
+ * @global $fp5_options Array of all the fp5 Options
  * @return void
  */
-function edd_text_callback( $args ) {
-	global $edd_options;
+function fp5_text_callback( $args ) {
+	global $fp5_options;
 
-	if ( isset( $edd_options[ $args['id'] ] ) )
-		$value = $edd_options[ $args['id'] ];
+	if ( isset( $fp5_options[ $args['id'] ] ) )
+		$value = $fp5_options[ $args['id'] ];
 	else
 		$value = isset( $args['std'] ) ? $args['std'] : '';
 
 	$size = isset( $args['size'] ) && !is_null($args['size']) ? $args['size'] : 'regular';
-	$html = '<input type="text" class="' . $args['size'] . '-text" id="edd_settings_' . $args['section'] . '[' . $args['id'] . ']" name="edd_settings_' . $args['section'] . '[' . $args['id'] . ']" value="' . esc_attr( $value ) . '"/>';
-	$html .= '<label for="edd_settings_' . $args['section'] . '[' . $args['id'] . ']"> '  . $args['desc'] . '</label>';
+	$html = '<input type="text" class="' . $args['size'] . '-text" id="fp5_settings_' . $args['section'] . '[' . $args['id'] . ']" name="fp5_settings_' . $args['section'] . '[' . $args['id'] . ']" value="' . esc_attr( $value ) . '"/>';
+	$html .= '<label for="fp5_settings_' . $args['section'] . '[' . $args['id'] . ']"> '  . $args['desc'] . '</label>';
 
 	echo $html;
 }
@@ -249,60 +249,16 @@ function edd_text_callback( $args ) {
  *
  * @since 1.0
  * @param array $args Arguments passed by the setting
- * @global $edd_options Array of all the EDD Options
+ * @global $fp5_options Array of all the fp5 Options
  * @return void
  */
-function edd_textarea_callback( $args ) {
-	global $edd_options;
+function fp5_textarea_callback( $args ) {
+	global $fp5_options;
 
-	if ( isset( $edd_options[ $args['id'] ] ) )
-		$value = $edd_options[ $args['id'] ];
-	else
-		$value = isset( $args['std'] ) ? $args['std'] : '';
-
-	$size = isset( $args['size'] ) && !is_null($args['size']) ? $args['size'] : 'regular';
-	$html = '<textarea class="large-text" cols="50" rows="5" id="edd_settings_' . $args['section'] . '[' . $args['id'] . ']" name="edd_settings_' . $args['section'] . '[' . $args['id'] . ']">' . esc_textarea( $value ) . '</textarea>';
-	$html .= '<label for="edd_settings_' . $args['section'] . '[' . $args['id'] . ']"> '  . $args['desc'] . '</label>';
-
-	echo $html;
-}
-
-/**
- * Password Callback
- *
- * Renders password fields.
- *
- * @since 1.3
- * @param array $args Arguments passed by the setting
- * @global $edd_options Array of all the EDD Options
- * @return void
+fp5 * @return void
  */
-function edd_password_callback( $args ) {
-	global $edd_options;
-
-	if ( isset( $edd_options[ $args['id'] ] ) )
-		$value = $edd_options[ $args['id'] ];
-	else
-		$value = isset( $args['std'] ) ? $args['std'] : '';
-
-	$size = isset( $args['size'] ) && !is_null($args['size']) ? $args['size'] : 'regular';
-	$html = '<input type="password" class="' . $args['size'] . '-text" id="edd_settings_' . $args['section'] . '[' . $args['id'] . ']" name="edd_settings_' . $args['section'] . '[' . $args['id'] . ']" value="' . esc_attr( $value ) . '"/>';
-	$html .= '<label for="edd_settings_' . $args['section'] . '[' . $args['id'] . ']"> '  . $args['desc'] . '</label>';
-
-	echo $html;
-}
-
-/**
- * Missing Callback
- *
- * If a function is missing for settings callbacks alert the user.
- *
- * @since 1.3.1
- * @param array $args Arguments passed by the setting
- * @return void
- */
-function edd_missing_callback($args) {
-	printf( __( 'The callback function used for the <strong>%s</strong> setting is missing.', 'edd' ), $args['id'] );
+function fp5_missing_callback($args) {
+	printf( __( 'The callback function used for the <strong>%s</strong> setting is missing.', 'fp5' ), $args['id'] );
 }
 
 /**
@@ -312,21 +268,21 @@ function edd_missing_callback($args) {
  *
  * @since 1.0
  * @param array $args Arguments passed by the setting
- * @global $edd_options Array of all the EDD Options
+ * @global $fp5_options Array of all the fp5 Options
  * @return void
  */
-function edd_select_callback($args) {
-	global $edd_options;
+function fp5_select_callback($args) {
+	global $fp5_options;
 
-	$html = '<select id="edd_settings_' . $args['section'] . '[' . $args['id'] . ']" name="edd_settings_' . $args['section'] . '[' . $args['id'] . ']"/>';
+	$html = '<select id="fp5_settings_' . $args['section'] . '[' . $args['id'] . ']" name="fp5_settings_' . $args['section'] . '[' . $args['id'] . ']"/>';
 
 	foreach ( $args['options'] as $option => $name ) :
-		$selected = isset( $edd_options[ $args['id'] ] ) ? selected( $option, $edd_options[$args['id']], false ) : '';
+		$selected = isset( $fp5_options[ $args['id'] ] ) ? selected( $option, $fp5_options[$args['id']], false ) : '';
 		$html .= '<option value="' . $option . '" ' . $selected . '>' . $name . '</option>';
 	endforeach;
 
 	$html .= '</select>';
-	$html .= '<label for="edd_settings_' . $args['section'] . '[' . $args['id'] . ']"> '  . $args['desc'] . '</label>';
+	$html .= '<label for="fp5_settings_' . $args['section'] . '[' . $args['id'] . ']"> '  . $args['desc'] . '</label>';
 
 	echo $html;
 }
@@ -338,22 +294,22 @@ function edd_select_callback($args) {
  *
  * @since 1.0
  * @param array $args Arguments passed by the setting
- * @global $edd_options Array of all the EDD Options
+ * @global $fp5_options Array of all the fp5 Options
  * @return void
  */
-function edd_upload_callback($args) {
-	global $edd_options;
+function fp5_upload_callback($args) {
+	global $fp5_options;
 
-	if ( isset( $edd_options[ $args['id'] ] ) )
-		$value = $edd_options[$args['id']];
+	if ( isset( $fp5_options[ $args['id'] ] ) )
+		$value = $fp5_options[$args['id']];
 	else
 		$value = isset($args['std']) ? $args['std'] : '';
 
 	$size = isset( $args['size'] ) && !is_null( $args['size'] ) ? $args['size'] : 'regular';
 
-	$html = '<input type="text" class="' . $args['size'] . '-text edd_upload_field" id="edd_settings_' . $args['section'] . '[' . $args['id'] . ']" name="edd_settings_' . $args['section'] . '[' . $args['id'] . ']" value="' . esc_attr( $value ) . '"/>';
-	$html .= '<span>&nbsp;<input type="button" class="edd_settings_upload_button button-secondary" value="' . __( 'Upload Logo', 'edd' ) . '"/></span>';
-	$html .= '<label for="edd_settings_' . $args['section'] . '[' . $args['id'] . ']"> '  . $args['desc'] . '</label>';
+	$html = '<input type="text" class="' . $args['size'] . '-text fp5_upload_field" id="fp5_settings_' . $args['section'] . '[' . $args['id'] . ']" name="fp5_settings_' . $args['section'] . '[' . $args['id'] . ']" value="' . esc_attr( $value ) . '"/>';
+	$html .= '<span>&nbsp;<input type="button" class="fp5_settings_upload_button button-secondary" value="' . __( 'Upload Logo', 'fp5' ) . '"/></span>';
+	$html .= '<label for="fp5_settings_' . $args['section'] . '[' . $args['id'] . ']"> '  . $args['desc'] . '</label>';
 
 	echo $html;
 }
@@ -368,8 +324,8 @@ function edd_upload_callback($args) {
  * @param array $input The value inputted in the field
  * @return string $input Sanitizied value
  */
-function edd_settings_sanitize( $input ) {
-	add_settings_error( 'edd-notices', '', __('Settings Updated', 'edd'), 'updated' );
+function fp5_settings_sanitize( $input ) {
+	add_settings_error( 'fp5-notices', '', __('Settings Updated', 'fp5'), 'updated' );
 	return $input;
 }
 
@@ -379,10 +335,10 @@ function edd_settings_sanitize( $input ) {
  * Retrieves all plugin settings and returns them as a combined array.
  *
  * @since 1.0
- * @return array Merged array of all the EDD settings
+ * @return array Merged array of all the fp5 settings
  */
-function edd_get_settings() {
-	$general_settings = is_array( get_option( 'edd_settings_general' ) ) ? get_option( 'edd_settings_general' )  : array();
+function fp5_get_settings() {
+	$general_settings = is_array( get_option( 'fp5_settings_general' ) ) ? get_option( 'fp5_settings_general' )  : array();
 
 	return array_merge( $general_settings );
 }
