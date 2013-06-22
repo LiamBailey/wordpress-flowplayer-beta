43870964feabdfd52d6adf072ca7601061e203a8
Ulrich Pogson <grapplerulrich@gmail.com>
2013-06-23 00:20:33 +0200

    -

--- class-flowplayer.php
+++ class-flowplayer.php
@@ -308,7 +308,7 @@ class Flowplayer5 {
 	 */
 	public function display_plugin_admin_page() {
 		include_once( 'includes/display-settings.php' );
-		$edd_options = edd_get_settings();
+		$edd_options = fp5_get_settings();
 	}
 
 	/**
