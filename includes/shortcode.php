624588f8e49d3eab4b22cac775943e27be1bd304
Ulrich Pogson <grapplerulrich@gmail.com>
2013-06-30 08:46:34 +0200

    test

--- includes/shortcode.php
+++ includes/shortcode.php
@@ -38,7 +38,7 @@ global $post;
 	$logo          = $options['logo'];
 	$ga_account_id = $options['ga_account_id'];
 	$logo_origin   = $options['logo_origin'];
-	$cdn           = ( isset( $options['cdn_option'] ) ? $options['cdn_option'] : '' );
+	$cdn           = ( isset( $options['cdn_option'] ) ? '' : $options['cdn_option'] );
 
 	// Checks and displays the retrieved value
 	if( isset( $id ) ) {
