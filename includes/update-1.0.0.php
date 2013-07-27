c39c352fd2d056e462b13029b05a3179330d21e2
Ulrich Pogson <grapplerulrich@gmail.com>
2013-07-27 22:33:08 +0200

    update update code for shortcode 1.0.0

--- includes/update-1.0.0.php
+++ includes/update-1.0.0.php
@@ -34,10 +34,10 @@ update_option( 'fp5_settings_general', $new_options );
 // Delte old array
 delete_option( 'fp5_options' );
 
-
+add_action( 'admin_init', 'convert_video_shortcode' );
 
 // this needs to be completed... now standalone used to be included in the register shortcode script
-function convert_video_shortcode($shortcode_array){/*  run the conversion script on the post on the fly.
+function convert_video_shortcode( $shortcode_array ){/*  run the conversion script on the post on the fly.
 
 	[flowplayer splash="http://flowplayer.grappler.tk/files/2013/02/trailer_1080p.jpg"
 	webm="http://flowplayer.grappler.tk/files/2013/02/trailer_1080p.webm"
@@ -147,44 +147,74 @@ function add_meta_to_fp5_video( $vid_post_id, $old_shortcode_atts ){
 	// run through shortcode and add meta
 	foreach( $old_shortcode_atts as $key => $value ){
 
-		if( $key != 'mp4' && $key != 'ogg' && $key != 'webm' && $key != 'splash' ){
-			add_post_meta( $vid_post_id, $key, $value, $unique );
-		}
-
-	// add the splash image as the featured image for post
-		if( $key = 'splash' ){
-
-
-			// get file data
-			$wp_filetype = wp_check_filetype(basename($value), null );
-
-			$wp_upload_dir = wp_upload_dir();
-
-			$attachment = array(
-				'guid' => $wp_upload_dir['url'] . '/' . basename( $value ), 
-				'post_mime_type' => $wp_filetype['type'],
-				'post_title' => preg_replace('/\.[^.]+$/', '', basename($value)),
-				'post_content' => '',
-				'post_status' => 'inherit'
-			);
-			$attach_id = wp_insert_attachment( $attachment, $value, 37 );
-			// you must first include the image.php file
-			// for the function wp_generate_attachment_metadata() to work
-			require_once(ABSPATH . 'wp-admin/includes/image.php');
-			$attach_data = wp_generate_attachment_metadata( $attach_id, $value );
-			wp_update_attachment_metadata( $attach_id, $attach_data );
-
-		}
+		add_post_meta( $vid_post_id, $key, $value, $unique );
 
 	} // end foreach $old_shortcode
 
 	//check for missing post meta
-	$fp5_ratio = get_post_meta( $vid_post_id, 'fp5[ratio]', true );
-	if( !isset( $fp5_ratio ) ){
-		add_post_meta( $vid_post_id, 'fp5[ratio]', '', $unique );
+	$fp5_mp4_video = get_post_meta( $vid_post_id, 'fp5-mp4-video', true );
+	if( !isset( $fp5_mp4_video ) ){
+		add_post_meta( $vid_post_id, 'fp5-mp4-video', $unique );
 	}
 
-	// $ratio = ($width != '' && $height != '' ? intval($height) / intval($width) : '');
+	//check for missing post meta
+	$fp5_webm_video = get_post_meta( $vid_post_id, 'fp5-webm-video', true );
+	if( !isset( $fp5_webm_video ) ){
+		add_post_meta( $vid_post_id, 'fp5-webm-video', $unique );
+	}
+
+	//check for missing post meta
+	$fp5_ogg_video = get_post_meta( $vid_post_id, 'fp5-ogg-video', true );
+	if( !isset( $fp5_ogg_video ) ){
+		add_post_meta( $vid_post_id, 'fp5-ogg-video', $unique );
+	}
+
+	$fp5_vtt_subtitles = get_post_meta( $vid_post_id, 'fp5-vtt-subtitles', true );
+	if( !isset( $fp5_vtt_subtitles ) ){
+		add_post_meta( $vid_post_id, 'fp5-vtt-subtitles', $unique );
+	}
+
+	//check for missing post meta
+	$fp5_select_skin = get_post_meta( $vid_post_id, 'fp5-select-skin', true );
+	if( !isset( $fp5_select_skin ) ){
+		add_post_meta( $vid_post_id, 'fp5-select-skin', $unique );
+	}
+
+	//check for missing post meta
+	$fp5_splash_image = get_post_meta( $vid_post_id, 'fp5-splash-image', true );
+	if( !isset( $fp5_splash_image ) ){
+		add_post_meta( $vid_post_id, 'fp5-splash-image', $unique );
+	}
+
+	//check for missing post meta
+	$fp5_autoplay = get_post_meta( $vid_post_id, 'fp5-autoplay', true );
+	if( !isset( $fp5_autoplay ) ){
+		add_post_meta( $vid_post_id, 'fp5-autoplay', $unique );
+	}
+
+	//check for missing post meta
+	$fp5_loop = get_post_meta( $vid_post_id, 'fp5-loop', true );
+	if( !isset( $fp5_loop ) ){
+		add_post_meta( $vid_post_id, 'fp5-loop', $unique );
+	}
+
+	//check for missing post meta
+	$fp5_width = get_post_meta( $vid_post_id, 'fp5-width', true );
+	if( !isset( $fp5_width ) ){
+		add_post_meta( $vid_post_id, 'fp5-width', $unique );
+	}
+
+	//check for missing post meta
+	$fp5_height = get_post_meta( $vid_post_id, 'fp5-height', true );
+	if( !isset( $fp5_height ) ){
+		add_post_meta( $vid_post_id, 'fp5-height', $unique );
+	}
+
+	//check for missing post meta
+	$fp5_fixed_width = get_post_meta( $vid_post_id, 'fp5-fixed-width', true );
+	if( !isset( $fp5_fixed_width ) ){
+		add_post_meta( $vid_post_id, 'fp5-fixed-width', $unique );
+	}
 
 }
 
