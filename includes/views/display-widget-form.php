85c2d5cc0460199ed62227deac25ae36b38f4996
Ulrich Pogson <grapplerulrich@gmail.com>
2013-11-29 22:05:59 +0100

    Update

--- includes/views/display-widget-form.php
+++ includes/views/display-widget-form.php
@@ -1,30 +1,18 @@
-<p><?php echo __( 'Use the dropdown below to chose a video.', 'flowplayer5' ); ?></p>
-<div>
-	<select name="flowplayer5_video_id" id="flowplayer5_video_id">
-		<?php
-		global $post;
-		$args = array(
-			'posts_per_page' => -1,
-			'post_type'      => 'flowplayer5'
-		);
-		$posts = get_posts( $args );
-		foreach( $posts as $post ) : setup_postdata( $post );
-		?>
-		<option value="<? echo $post->ID; ?>" id="<? echo $post->ID; ?>"><?php the_title(); ?></option>
-		<?php endforeach; ?>
-	</select>
-</div>
+<p><?php echo __( 'Chose a video from the dropdown.', 'flowplayer5' ); ?></p>
 <?php
 // WP_Query arguments
 $args = array (
 	'post_type'              => 'flowplayer5',
-	'post_status'            => 'published',
+	'post_status'            => 'publish',
 );
 
 // The Query
-$posts = new WP_Query( $args );
-echo $posts;
+$query = new WP_Query( $args );
+$posts = $query->posts;
+//print_r( $posts);
+echo '<select name="flowplayer5_video_id" id="flowplayer5_video_id">';
 foreach ( $posts as $post ) {
 	echo '<option value="' . $post->ID . '" id="' . $post->ID . '"', $select == $option ? ' selected="selected"' : '', '>' . $post->post_title . '</option>';
 }
+echo '</select>';
 ?>
