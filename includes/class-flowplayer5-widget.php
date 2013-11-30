3f99d33e89e560851920df7080bdc1f18139acae
Ulrich Pogson <grapplerulrich@gmail.com>
2013-11-30 10:02:24 +0100

    update

--- includes/class-flowplayer5-widget.php
+++ includes/class-flowplayer5-widget.php
@@ -35,63 +35,23 @@ class Flowplayer5_Widget extends WP_Widget {
 		// Call $plugin_slug from public plugin class.
 		$this->plugin_slug = $plugin->get_plugin_slug();
 
+		/**
+		 * Default widget option values.
+		 */
+		$this->defaults = array(
+			'id' => '3'
+		);
+
+		$widget_ops = array(
+			'classname'   => 'flowplayer5-video-widget',
+			'description' => __( 'Display your Flowplayer Videos in a Widget.', $this->plugin_slug )
+		);
+
 		parent::__construct(
 				'flowplayer5-video-widget',
 				__( 'Flowplayer Video Widget', $this->plugin_slug ),
-				array(
-						'classname'   => 'flowplayer5-video-widget',
-						'description' => __( 'Display your Flowplayer Videos in a Widget.', $this->plugin_slug )
-				)
+				$widget_ops
 		);
-
-	}
-
-	/**
-	 * Admin side Widget form.
-	 *
-	 * @since    1.4.0
-	 */
- 	public function form( $instance ) {
-
-		// outputs the options form on admin
-		$instance = wp_parse_args(
-			( array ) $instance
-		);
-
-		$html = '<p>' __( 'Chose a video from the dropdown.', $this->plugin_slug ) '</p>';
-
-		// WP_Query arguments
-		$args = array (
-			'post_type'              => 'flowplayer5',
-			'post_status'            => 'publish',
-		);
-
-		// The Query
-		$query = new WP_Query( $args );
-		$posts = $query->posts;
-
-		$html .= '<select name="flowplayer5_video_id" id="flowplayer5_video_id">';
-		foreach ( $posts as $post ) {
-			$html .= '<option value="' . $post->ID . '" id="' . $post->ID . '"', $select == $option ? ' selected="selected"' : '', '>' . $post->post_title . '</option>';
-		}
-		$html .= '</select>';
-
-		echo $html;
-
-	}
-
-	/**
-	 * Update widget settings.
-	 *
-	 * @since    1.4.0
-	 */
-	public function update( $new_instance, $old_instance ) {
-		// processes widget options to be saved
-		$instance = $old_instance;
-
-		$instance['flowplayer5_video_id'] = esc_attr( $new_instance['flowplayer5_video_id'] );
-
-		return $instance;
 
 	}
 
@@ -103,8 +63,8 @@ class Flowplayer5_Widget extends WP_Widget {
 	public function widget( $args, $instance ) {
 		// outputs the content of the widget
 		extract( $args, EXTR_SKIP );
-		
-		$id    = $instance['flowplayer5_video_id'];
+
+		$id    = $instance['id'];
 		$title = get_the_title( $id );
 		echo $before_widget;
 		echo '<h3 class="widget-title">' . $title . '</h3>';
@@ -113,6 +73,69 @@ class Flowplayer5_Widget extends WP_Widget {
 		echo $after_widget;
 	}
 
+	/**
+	 * Update widget settings.
+	 *
+	 * @since    1.4.0
+	 */
+	public function update( $new_instance, $old_instance ) {
+
+		echo $new_instance['id'];
+		$instance['id'] = esc_attr( $new_instance['id'] );
+
+		return $instance;
+
+	}
+
+	/**
+	 * Admin side Widget form.
+	 *
+	 * @since    1.4.0
+	 */
+	public function form( $instance ) {
+
+		// Merge with defaults
+		$instance = wp_parse_args(
+			( array ) $instance,
+			$this->defaults
+		);
+
+		$id = isset( $instance['id']) ? esc_attr( $instance['id'] ) : '';
+
+		// WP_Query arguments
+		$args = array (
+			'post_type'              => 'flowplayer5',
+			'post_status'            => 'publish',
+		);
+
+		// The Query
+		$query = new WP_Query( $args );
+		$posts = $query->posts;
+
+		$html = '<p><label for="' . $this->get_field_id( 'id' ) . '">' . __( 'Chose a video from the dropdown.', $this->plugin_slug ) . '</label>';
+		$html .= '<select class="widefat" name="' . $this->get_field_id( 'id' ) . '" id="' . $this->get_field_name( 'id' ) . '">';
+		foreach ( $posts as $post ) {
+			$html .= '<option value="' . $post->ID . '"' . selected( $post->ID, $id ) . '>' . $post->post_title . '</option>';
+		}
+		$html .= '</select></p>';
+
+		echo $html;
+
+	}
+
 }
 
-add_action( 'widgets_init', create_function( '', 'register_widget("Flowplayer5_Widget");' ) );
+/**
+ * Register the Flowplayer5 widgets on startup.
+ *
+ * Calls 'widgets_init' action after all of the WordPress widgets have been
+ * registered.
+ *
+ * @since 1.4.0
+ */
+function fp5_widgets_init() {
+
+	register_widget( 'Flowplayer5_Widget' );
+
+}
+add_action( 'widgets_init', 'fp5_widgets_init' );
